7507148afda7e1af69b21e09d9df762266d55026
robdy <robdyjas@gmail.com>
2020-04-22 19:29:41 +0200

    Add Netlify config

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+TODAY=$(date +%Y%m%d)
+MESSAGE="$TODAY page build"
+ 
+
+cd _site
+
+git init
+git config user.name "robdy"
+git config user.email $EMAIL
+
+if ! git remote | grep upstream > /dev/null; then
+git remote add upstream "https://$GITHUB_TOKEN@github.com/robdy/robdy.github.io.git"
+fi
+git fetch upstream
+git reset upstream/master
+
+touch .
+
+git add -A .
+git commit -m "$MESSAGE"
+git push -q upstream HEAD:master
