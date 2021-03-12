075a9985e18c2e173171e5cd287598c18bc32cec
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-03-12 20:03:06 +0100

    Add edit link to blog post (#130)
    * Add relativePath to blog post
    
    * Add link to blog post
    
    * Prettier changes

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
-import Comments from '../components/Comments'
-import useSiteMetadata from '../components/SiteMetadata'
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import Comments from "../components/Comments";
+import useSiteMetadata from "../components/SiteMetadata";
 
 export const BlogPostTemplate = ({
   content,
@@ -16,18 +16,19 @@ export const BlogPostTemplate = ({
   tags,
   title,
   helmet,
+  relativePath,
 }) => {
-  const PostContent = contentComponent || Content
+  const PostContent = contentComponent || Content;
 
   return (
     <section className="section">
-      {helmet || ''}
+      {helmet || ""}
       <div className="container content">
         <div className="header-container">
-                    {tags && tags.length ? (
+          {tags && tags.length ? (
             <div className="taglist-container">
               <ul className="taglist">
-                {tags.map(tag => (
+                {tags.map((tag) => (
                   <li key={tag + `tag`}>
                     <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
                   </li>
@@ -35,18 +36,31 @@ export const BlogPostTemplate = ({
               </ul>
             </div>
           ) : null}
-          <h1 className="post-title">
-            {title}
-          </h1>
-          <p className="post-subheader">By <Link className="post-subheader-link" to="/about">Robert Dyjas</Link> {date}</p>
+          <h1 className="post-title">{title}</h1>
+          <p className="post-subheader">
+            By{" "}
+            <Link className="post-subheader-link" to="/about">
+              Robert Dyjas
+            </Link>{" "}
+            {date}
+            &nbsp;&bull;&nbsp;
+            <a
+              href={`https://github.com/robdy/robdy.github.io/edit/src/src/pages/${relativePath}`}
+              className="post-subheader-link"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              Edit this post
+            </a>
+          </p>
         </div>
         <p className="description">{description}</p>
         <PostContent content={content} />
         <Comments />
       </div>
     </section>
-  )
-}
+  );
+};
 
 BlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
@@ -56,32 +70,32 @@ BlogPostTemplate.propTypes = {
   title: PropTypes.string,
   slug: PropTypes.string,
   helmet: PropTypes.object,
-}
+  relativePath: PropTypes.string,
+};
 
 const BlogPost = ({ data }) => {
   const { markdownRemark: post } = data;
   const { siteUrl } = useSiteMetadata();
 
-
   const units = {
     year: 24 * 60 * 60 * 1000 * 365,
-    month: 24 * 60 * 60 * 1000 * 365 / 12,
+    month: (24 * 60 * 60 * 1000 * 365) / 12,
     week: 24 * 60 * 60 * 1000 * 7,
     day: 24 * 60 * 60 * 1000,
     hour: 60 * 60 * 1000,
     minute: 60 * 1000,
-    second: 1000
-  }
+    second: 1000,
+  };
   const currentTimeStamp = new Date().getTime();
-  const elapsed = new Date(post.frontmatter.date) - currentTimeStamp
-  const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
+  const elapsed = new Date(post.frontmatter.date) - currentTimeStamp;
+  const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
   const relativeDate = (() => {
     for (let u in units) {
-      if ((Math.abs(elapsed) / units[u]) >= 1) {
-        return (rtf.format(Math.round(elapsed / units[u]), u));
+      if (Math.abs(elapsed) / units[u] >= 1) {
+        return rtf.format(Math.round(elapsed / units[u]), u);
       }
     }
-  })()
+  })();
 
   return (
     <Layout>
@@ -97,30 +111,39 @@ const BlogPost = ({ data }) => {
               name="description"
               content={`${post.frontmatter.description}`}
             />
-            <meta property="og:description" content={`${post.frontmatter.description}`} />
+            <meta
+              property="og:description"
+              content={`${post.frontmatter.description}`}
+            />
             <meta property="og:title" content={`${post.frontmatter.title}`} />
             <meta property="og:url" content={`${siteUrl}${post.fields.slug}`} />
           </Helmet>
         }
+        relativePath={post.parent.relativePath}
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
       />
     </Layout>
-  )
-}
+  );
+};
 
 BlogPost.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object,
   }),
-}
+};
 
-export default BlogPost
+export default BlogPost;
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
+      parent {
+        ... on File {
+          relativePath
+        }
+      }
       fields {
         slug
       }
@@ -133,4 +156,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
