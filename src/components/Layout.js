97ea9fed97aa0b559cf656990a98aee90bad04d9
Robert Dyjas <robdyjas@gmail.com>
2021-04-07 19:20:53 +0200

    Add Plausible

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import "./layout.css";
-import useSiteMetadata from "./SiteMetadata";
-import { withPrefix } from "gatsby";
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import './layout.css'
+import useSiteMetadata from './SiteMetadata'
+import { withPrefix } from 'gatsby'
 
 const TemplateWrapper = ({ children }) => {
-  const { title, description, siteUrl } = useSiteMetadata();
+  const { title, description, siteUrl } = useSiteMetadata()
   return (
     <div>
       <Helmet>
@@ -17,109 +17,109 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="apple-touch-icon-precomposed"
           sizes="144x144"
-          href={`${siteUrl}${withPrefix("/")}img/favicon.png`}
+          href={`${siteUrl}${withPrefix('/')}img/favicon.png`}
         />
         <link
           rel="shortcut icon"
-          href={`${siteUrl}${withPrefix("/")}img/favicon.ico`}
+          href={`${siteUrl}${withPrefix('/')}img/favicon.ico`}
         />
         {/* From favicomatic.com */}
         <link
           rel="apple-touch-icon-precomposed"
           sizes="57x57"
-          href={`${siteUrl}${withPrefix("/")}img/apple-touch-icon-57x57.png`}
+          href={`${siteUrl}${withPrefix('/')}img/apple-touch-icon-57x57.png`}
         />
         <link
           rel="apple-touch-icon-precomposed"
           sizes="114x114"
-          href={`${siteUrl}${withPrefix("/")}img/apple-touch-icon-114x114.png`}
+          href={`${siteUrl}${withPrefix('/')}img/apple-touch-icon-114x114.png`}
         />
         <link
           rel="apple-touch-icon-precomposed"
           sizes="72x72"
-          href={`${siteUrl}${withPrefix("/")}img/apple-touch-icon-72x72.png`}
+          href={`${siteUrl}${withPrefix('/')}img/apple-touch-icon-72x72.png`}
         />
         <link
           rel="apple-touch-icon-precomposed"
           sizes="144x144"
-          href={`${siteUrl}${withPrefix("/")}img/apple-touch-icon-144x144.png`}
+          href={`${siteUrl}${withPrefix('/')}img/apple-touch-icon-144x144.png`}
         />
         <link
           rel="apple-touch-icon-precomposed"
           sizes="60x60"
-          href={`${siteUrl}${withPrefix("/")}img/apple-touch-icon-60x60.png`}
+          href={`${siteUrl}${withPrefix('/')}img/apple-touch-icon-60x60.png`}
         />
         <link
           rel="apple-touch-icon-precomposed"
           sizes="120x120"
-          href={`${siteUrl}${withPrefix("/")}img/apple-touch-icon-120x120.png`}
+          href={`${siteUrl}${withPrefix('/')}img/apple-touch-icon-120x120.png`}
         />
         <link
           rel="apple-touch-icon-precomposed"
           sizes="76x76"
-          href={`${siteUrl}${withPrefix("/")}img/apple-touch-icon-76x76.png`}
+          href={`${siteUrl}${withPrefix('/')}img/apple-touch-icon-76x76.png`}
         />
         <link
           rel="apple-touch-icon-precomposed"
           sizes="152x152"
-          href={`${siteUrl}${withPrefix("/")}img/apple-touch-icon-152x152.png`}
+          href={`${siteUrl}${withPrefix('/')}img/apple-touch-icon-152x152.png`}
         />
         <link
           rel="icon"
           type="image/png"
           href={`${siteUrl}${withPrefix(
-            "/"
+            '/'
           )}img/favicon-196x196.png" sizes="196x196`}
         />
         <link
           rel="icon"
           type="image/png"
           href={`${siteUrl}${withPrefix(
-            "/"
+            '/'
           )}img/favicon-96x96.png" sizes="96x96`}
         />
         <link
           rel="icon"
           type="image/png"
           href={`${siteUrl}${withPrefix(
-            "/"
+            '/'
           )}img/favicon-32x32.png" sizes="32x32`}
         />
         <link
           rel="icon"
           type="image/png"
           href={`${siteUrl}${withPrefix(
-            "/"
+            '/'
           )}img/favicon-16x16.png" sizes="16x16`}
         />
         <link
           rel="icon"
           type="image/png"
           href={`${siteUrl}${withPrefix(
-            "/"
+            '/'
           )}img/favicon-128.png" sizes="128x128`}
         />
         <meta name="application-name" content="&nbsp;" />
         <meta name="msapplication-TileColor" content="#FFFFFF" />
         <meta
           name="msapplication-TileImage"
-          content={`${siteUrl}${withPrefix("/")}img/mstile-144x144.png`}
+          content={`${siteUrl}${withPrefix('/')}img/mstile-144x144.png`}
         />
         <meta
           name="msapplication-square70x70logo"
-          content={`${siteUrl}${withPrefix("/")}img/mstile-70x70.png`}
+          content={`${siteUrl}${withPrefix('/')}img/mstile-70x70.png`}
         />
         <meta
           name="msapplication-square150x150logo"
-          content={`${siteUrl}${withPrefix("/")}img/mstile-150x150.png`}
+          content={`${siteUrl}${withPrefix('/')}img/mstile-150x150.png`}
         />
         <meta
           name="msapplication-wide310x150logo"
-          content={`${siteUrl}${withPrefix("/")}img/mstile-310x150.png`}
+          content={`${siteUrl}${withPrefix('/')}img/mstile-310x150.png`}
         />
         <meta
           name="msapplication-square310x310logo"
-          content={`${siteUrl}${withPrefix("/")}img/mstile-310x310.png`}
+          content={`${siteUrl}${withPrefix('/')}img/mstile-310x310.png`}
         />
         <meta name="theme-color" content="#fff" />
 
@@ -131,15 +131,21 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${siteUrl}${withPrefix("/")}img/og-image.png`}
+          content={`${siteUrl}${withPrefix('/')}img/og-image.png`}
         />
         <meta property="og:description" content={description} />
+        <script
+          async
+          defer
+          data-domain="robdy.io"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
       </Helmet>
       <Navbar />
       {children}
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default TemplateWrapper;
+export default TemplateWrapper
