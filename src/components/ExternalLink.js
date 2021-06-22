e36253cd5449635330eda122ac46c938df4d2a82
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-06-22 23:04:33 +0200

    Rewrite about page to JSX (#230)

---
+++ src/components/ExternalLink.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ExternalLink(props) {
+  return (
+    <React.Fragment>
+      <a href={props.href} target="_blank" rel="nofollow noopener noreferrer">
+        {props.children}
+      </a>
+    </React.Fragment>
+  )
+}
+
+export default ExternalLink
