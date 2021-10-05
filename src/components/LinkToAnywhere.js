7ab99887abe1033f32041492fdc6916a784b982c
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-10-05 16:13:00 +0200

    Custom component for MDX links (#319)
    * Add LinkToAnywhere component
    
    * Paragraph links support
    
    * Remove external links package

---
+++ src/components/LinkToAnywhere.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import useSiteMetadata from './SiteMetadata'
+
+export const LinkToAnywhere = (props) => {
+  const { siteUrl } = useSiteMetadata()
+  console.log({ props })
+
+  // Internal links
+  if (
+    // Handling links with hardcoded domain name
+    (props.href.startsWith(siteUrl) && process.env.NODE_ENV === 'production') ||
+    props.href.startsWith('/')
+  ) {
+    return (
+      <Link to={props.href} aria-label={`${props.children} link`}>
+        {props.children}
+      </Link>
+    )
+  }
+
+  // Paragraph links
+  if (props.href.startsWith('#')) {
+    return (
+      <Link
+        className="anchor before"
+        aria-label={props['aria-label']}
+        to={props.href}
+      >
+        {props.children}
+      </Link>
+    )
+  }
+
+  // External links
+  return (
+    <a href={props.href} rel={props.rel} target={props.target}>
+      {props.children}
+    </a>
+  )
+}
