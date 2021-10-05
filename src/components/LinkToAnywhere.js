6d2b6ec7dc53529e0e7df059ea4bf4119a8ee33e
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-10-05 14:03:46 +0000

    Paragraph links support

--- src/components/LinkToAnywhere.js
+++ src/components/LinkToAnywhere.js
@@ -4,13 +4,38 @@ import useSiteMetadata from './SiteMetadata'
 
 export const LinkToAnywhere = (props) => {
   const { siteUrl } = useSiteMetadata()
-  // Check paragraph links
+  console.log({ props })
+
+  // Internal links
   if (
     // Handling links with hardcoded domain name
     (props.href.startsWith(siteUrl) && process.env.NODE_ENV === 'production') ||
     props.href.startsWith('/')
   ) {
-    return <Link to={props.href}>{props.children}</Link>
+    return (
+      <Link to={props.href} aria-label={`${props.children} link`}>
+        {props.children}
+      </Link>
+    )
   }
-  return <a {...props} />
+
+  // Paragraph links
+  if (props.href.startsWith('#')) {
+    return (
+      <Link
+        className="anchor before"
+        aria-label={props['aria-label']}
+        to={props.href}
+      >
+        {props.children}
+      </Link>
+    )
+  }
+
+  // External links
+  return (
+    <a href={props.href} rel={props.rel} target={props.target}>
+      {props.children}
+    </a>
+  )
 }
