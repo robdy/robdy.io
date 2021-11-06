ca204222660694bf463f02ef32253c34693af9b6
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-11-06 12:05:54 +0000

    Change RSS opacity

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,33 +1,11 @@
-// Taken from https://dev.to/vincentntang/installing-gatsbyjs-blog-comments-using-utterances-1h8j
+import React from 'react'
 
-import React, { Component } from 'react'
-// import ThemeContext from '../context/ThemeContext';
-export default class Comments extends Component {
-  // static contextType = ThemeContext;
-
-  constructor(props) {
-    super(props)
-    this.commentBox = React.createRef() // Creates a reference to inject the <script> element
-  }
-  componentDidMount() {
-    // const utteranceTheme = theme.dark ? "github-dark" : "github-light";
-    let scriptEl = document.createElement('script')
-    scriptEl.setAttribute('src', 'https://utteranc.es/client.js')
-    scriptEl.setAttribute('crossorigin', 'anonymous')
-    scriptEl.setAttribute('async', true)
-    scriptEl.setAttribute('repo', 'robdy/blog-comments')
-    scriptEl.setAttribute('issue-term', 'pathname')
-    // scriptEl.setAttribute("theme", utteranceTheme);
-    scriptEl.setAttribute('theme', 'github-light')
-    this.commentBox.current.appendChild(scriptEl)
-  }
-
-  render() {
-    return (
-      <div>
-        <div ref={this.commentBox} className="comment-box" />
-        {/* Above element is where the comments are injected */}
-      </div>
-    )
-  }
+function Comments() {
+  return (
+    <div>
+      sss
+    </div>
+  )
 }
+
+export default Comments
