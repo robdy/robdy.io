14afdc815f11e17ee58fc7ded238a6323edcfb48
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-12-16 20:12:20 +0000

    Change all posts header

--- src/pages/all/index.js
+++ src/pages/all/index.js
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet'
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 import Navbar from '../../components/Navbar'
+import rss from '../../img/rss-solid.svg'
 
 export default class BlogIndexPage extends React.Component {
   render() {
@@ -13,7 +14,13 @@ export default class BlogIndexPage extends React.Component {
         </Helmet>
         <div className="container">
           <Navbar />
-          <h1 className="page-header">All posts</h1>
+						<div className="flex newest-posts-container">
+							<h2 className="newest-posts-header">All posts</h2>
+							<a href="rss.xml" className="rss-link" rel="nofollow noopener noreferrer" target="_blank">
+								<img src={rss} className="rss-icon" alt="RSS icon" />
+							</a>
+						</div>
+
         </div>
         <section className="section">
           <div className="container">
