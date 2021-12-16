9e7ade910aa4b6fe0ec3ed761b09b146dc1b0e25
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-12-16 12:28:57 +0000

    Fix mobile header, RSS icon, add comments back

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,11 +1,34 @@
-import React from 'react'
+// Taken from https://dev.to/vincentntang/installing-gatsbyjs-blog-comments-using-utterances-1h8j
 
-function Comments() {
-  return (
-    <div>
-      sss
-    </div>
-  )
+import React, { Component } from "react";
+// import ThemeContext from '../context/ThemeContext';
+export default class Comments extends Component {
+  // static contextType = ThemeContext;
+
+  constructor(props) {
+    super(props);
+    this.commentBox = React.createRef(); // Creates a reference to inject the <script> element
+  }
+  componentDidMount() {
+    const theme = this.context;
+    // const utteranceTheme = theme.dark ? "github-dark" : "github-light";
+    let scriptEl = document.createElement("script");
+    scriptEl.setAttribute("src", "https://utteranc.es/client.js");
+    scriptEl.setAttribute("crossorigin", "anonymous");
+    scriptEl.setAttribute("async", true);
+    scriptEl.setAttribute("repo", "robdy/blog-comments");
+    scriptEl.setAttribute("issue-term", "pathname");
+    // scriptEl.setAttribute("theme", utteranceTheme);
+    scriptEl.setAttribute("theme", "github-light");
+    this.commentBox.current.appendChild(scriptEl);
+  }
+
+  render() {
+    return (
+      <div>
+        <div ref={this.commentBox} className="comment-box" />
+        {/* Above element is where the comments are injected */}
+      </div>
+    );
+  }
 }
-
-export default Comments
