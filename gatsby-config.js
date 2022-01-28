b68f7fbf9e539f4848c65c004f2edbe20e31e1b2
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2022-01-28 08:11:10 +0100

    Removes ToC plugin (#420)
    * Remove ToC plugin and update articles
    
    * Update package-lock.json
    
    * Add missing ToC
    
    * ToC changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,16 +56,6 @@ module.exports = {
               destinationDir: 'static',
             },
           },
-          {
-            resolve: 'gatsby-remark-table-of-contents',
-            options: {
-              exclude: 'Table of Contents',
-              tight: false,
-              fromHeading: 1,
-              toHeading: 6,
-              className: 'table-of-contents',
-            },
-          },
           'gatsby-remark-autolink-headers',
           {
             resolve: `gatsby-remark-prismjs`,
