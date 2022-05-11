cb572830961e8669b317e193d9d6a2b8811f40c2
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2022-05-11 20:09:29 +0000

    Code header styling

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -4,10 +4,16 @@ function CodeBlock(props) {
 	const languageString = props.className.replace(/(language-| line-numbers)/mg,'');
 	return (
 		<React.Fragment>
-			<div className='codeHeader'>{languageString}</div>
-			<pre {...props}>
-				{props.children}
-			</pre>
+			<section className='codeheader-section'>
+				<div className='codeheader flex'>
+					<span className='codeheader-language'>{languageString}</span>
+					<button className='codeheader-button'>Wrap</button>
+					<button className='codeheader-button'>Copy</button>
+				</div>
+				<pre {...props}>
+					{props.children}
+				</pre>
+			</section>
 		</React.Fragment>
 	)
 }
