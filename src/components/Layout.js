cced21342ae372a900be99d47fe6cc5ec71f5954
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-02-22 15:20:51 +0000

    Fix header anchors

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import { withPrefix } from 'gatsby'
 import { MDXProvider } from '@mdx-js/react'
 import { NoteBlock, WarningBlock, TipBlock } from './Block'
 import { LinkToAnywhere } from './LinkToAnywhere'
-import { H2 } from './Headers'
+import { Header } from './Header'
 
 const TemplateWrapper = ({ children }) => {
   const { title, description, siteUrl } = useSiteMetadata()
@@ -151,7 +151,12 @@ const TemplateWrapper = ({ children }) => {
           Warning: WarningBlock,
           Tip: TipBlock,
           a: LinkToAnywhere,
-          h2: H2,
+          h1: props => <Header level={'1'} {...props} />,
+          h2: props => <Header level={'2'} {...props} />,
+          h3: props => <Header level={'3'} {...props} />,
+          h4: props => <Header level={'4'} {...props} />,
+          h5: props => <Header level={'5'} {...props} />,
+          h6: props => <Header level={'6'} {...props} />,
         }}
       >
         {children}
