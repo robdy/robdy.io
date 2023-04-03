0c601990cdfc0c2934c1b31104383681a45ab1d2
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-04-03 10:45:27 +0200

    Fixes Edit this post button (#773)

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -38,7 +38,7 @@ const BlogPostTemplate = ({
             <time datetime={isoDate}>{formattedDate}</time>
             &nbsp;&bull;&nbsp;
             <a
-              href={`https://github.com/robdy/robdy.io/edit/src/src/pages/${relativePath}`}
+              href={`https://github.com/robdy/robdy.io/edit/src/blog/${relativePath}`}
               className="post-subheader-link"
               target="_blank"
               rel="nofollow noopener noreferrer"
