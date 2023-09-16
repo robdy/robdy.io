3ca7130e8e2fdd15a9734436c5fb5a1b7e0ced16
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2023-09-16 14:19:42 +0200

    Updates About page (#20)

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -10,8 +10,7 @@ const AboutPage = () => (
       <Navbar />
       <h1>About me</h1>
       <p>
-        My name is Robert Dyjas. I'm Microsoft Teams and Skype for Business
-        specialist who also likes to learn{' '}
+        My name is Robert Dyjas. I'm Microsoft Teams and Microsoft 365 specialist who also likes to learn{' '}
         <code class="language-text">$newThings</code> in the meantime.
       </p>
       <p>
@@ -19,13 +18,69 @@ const AboutPage = () => (
         <code class="language-text">[JavaScript, Web Development, CSS]</code>.
         It might be subject to change frequently.
       </p>
+      <p>
+        Since the beginning of my career I'm working with Microsoft products. I'm mainly focused on communication tools: Teams, Skype for Business and Exchange.
+      </p>
+
+      <h2>Projects</h2>
+      <p>
+        I've been participating in various non-commercial projects in the past.
+      </p>
+      <h3>Microsoft 365 administration for Okręg Małopolski ZHR</h3>
+      <p>
+        I've been helping to implement new solutions based on Microsoft 365 for Polish non-profit organization.
+      </p>
+      <h3>Elekid bot</h3>
+      <p>
+        Discord bot written in Node.js.
+      </p>
+      <p>
+        Features include:
+        <ul>
+          <li>Responding to user commands</li>
+          <li>Setting nicknames on the server</li>
+          <li>Pulling social media entries and sending to predefined channels</li>
+          <li>Getting RSS entries from predefined URLs and sharing to predefined channels</li>
+          <li>Data caching</li>
+        </ul>
+      </p>
+      <p>
+        <ExternalLink href="https://github.com/robdy/elekid-bot">
+          GitHub repository
+        </ExternalLink>
+      </p>
+      <p>
+        <b>Technologies used</b>: Node.js, Discord.JS, Twitter API, PM2, Giphy API, Circle CI
+      </p>
+
+      <h3>FlairHQ</h3>
+      <p>
+        Web application to help moderating r/pokemontrades subreddit. I added new features (<ExternalLink href="https://github.com/pokemontrades/flairhq/pull/651">integration with Discord</ExternalLink>, <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/625">new type of notifications</ExternalLink>, <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/612">support for new games</ExternalLink>), fixed various bugs and added many small enhancements (
+        <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/629">#629</ExternalLink>,{' '}
+        <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/627">#627</ExternalLink>,{' '}
+        <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/597">#597</ExternalLink>,{' '}
+        <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/637">#637</ExternalLink>,{' '}
+        <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/631">#631</ExternalLink>,{' '}
+        <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/628">#628</ExternalLink>,{' '}
+        <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/645">#645</ExternalLink>) and <ExternalLink href="https://github.com/pokemontrades/flairhq/pull/599">added proper documentation</ExternalLink>.
+      </p>
+      <p>
+        FlairHQ was my first real application involving Node.js.
+      </p>
+      <p>
+        <ExternalLink href="https://github.com/pokemontrades/flairhq">GitHub repository</ExternalLink>
+      </p>
+      <p>
+        <b>Technologies used</b>: Git, Node.js, Sails.JS, Reddit API, Discord.JS
+      </p>
+                                                                                            
       <h2>Blog</h2>
       <p>
         On this blog I include some random hints/instructions about the issues I
         have resolved and decided to note so I don't have to search for the
         information once again. I'm glad if you find it somewhat useful!
       </p>
-      <h2>Credits</h2>
+      <h3>Credits</h3>
       <ul>
         <li>
           Built and hosted by {' '}
